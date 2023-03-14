ce7c3f7972c630db5bc37229334383386804cd7f
Muhamad Yusuf <muhamadyusuf0012@gmail.com>
2023-03-14 19:00:24 +0700

    skema db

---
+++ praktik_bi.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Localhost
+ Source Server Type    : MySQL
+ Source Server Version : 100421
+ Source Host           : localhost:3306
+ Source Schema         : praktik_bi
+
+ Target Server Type    : MySQL
+ Target Server Version : 100421
+ File Encoding         : 65001
+
+ Date: 14/03/2023 18:57:51
+*/
+
